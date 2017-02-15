1fc146bbe036b87d3ba2c1250b533c0d7930fa7f
Gloria Nlewedim <glnlewedim@davidson.edu>
2017-02-14 20:12:58 -0500

    pairing release for 4.3

---
+++ ruby/hamsters.rb
@@ -0,0 +1,21 @@
+puts "Hamster Name"
+  name=gets.chomp
+
+puts "Whats your volume level (1-10)?"
+  volume=gets.chomp.to_i
+
+puts "What is your fur color?"
+  color=gets.chomp
+
+puts "are you a good candidate for adoption? (true or false)"
+  candidate=gets.chomp
+
+puts "estimated age"
+  age=gets.chomp.to_i
+  #age=nil
+  if age==0
+     age= "N/A"
+    
+  end
+
+puts "Hey there! Your name is #{name}, you are a noise level of #{volume}, your fur color is #{color}, you are a #{candidate} candidate, and you are #{age} years old! Welcome!"
