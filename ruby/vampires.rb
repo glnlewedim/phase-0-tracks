0f1e512ec895c5314513f4a6d0985a90ead6a708
Gloria Nlewedim <glnlewedim@davidson.edu>
2017-02-17 14:58:40 -0500

    finish 4.4git add .git add .git add .

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -19,31 +19,29 @@ for i in (1..interview)
 
 	puts "Would you like to enroll in the company’s health insurance?"
 		health=gets.chomp
-end
+	if (name=="Drake Cula")||(name=="Tu Fang")&&(age1==age2)&&(age1==age3)&&(garlic_bread=="yes")&&(health=="yes")
+		puts "definietly a vampire"
 
+	elsif (age1==age2) || (age1==age3)&&(garlic_bread=="yes")||(health=="yes")
+		puts "probably not a vampire"
+	elsif (age1 != age2)&&(age1 !=age3)&&(garlic_bread == "no")&&(health == "no")
+		puts "almost certainly a vampire"
+	elsif (age1 != age2)&&(age1 != age3)&&(garlic_bread=="no")||(health=="no")
+		puts "probably a vampire"
+	else 
+		puts "results inconclusive"
+	end
 
-if (name=="Drake")||(name=="Tu Fang")&&(age1==age2)&&(age1==age3)&&(garlic_bread=="yes")&&(health=="yes")
-	puts "definietly a vampire"
+	if allergy = "undefined" 
+		until (allergy == "done") || (allergy == "sunshine") do
+		puts "Please enter all allergies, when you are finished with the survey type done" 
+		allergy = gets.chomp
 
-elsif (age1==age2) || (age1==age3)&&(garlic_bread=="yes")||(health=="yes")
-	puts "probably not a vampire"
-elsif (age1 != age2)&&(age1 !=age3)&&(garlic_bread == "no")&&(health == "no")
-	puts "almost certainly a vampire"
-elsif (age1 != age2)&&(age1 != age3)&&(garlic_bread=="no")||(health=="no")
-	puts "probably a vampire"
-else 
-	puts "results inconclusive"
-end
+	end
 
-if allergy = "undefined" 
-	until (allergy == "done") || (allergy == "sunshine") do
-	puts "Please enter all allergies, when you are finished with the survey type done" 
-	allergy = gets.chomp
-
-end
-
-if (allergy == "sunshine")
-	puts "probably a vampire"		
+	if (allergy == "sunshine")
+		puts "probably a vampire"		
+	end
 end
 
 
