abd042a66bf702ed28379229e7068949f5ffffbe
Gloria Nlewedim <glnlewedim@davidson.edu>
2017-02-20 23:21:58 -0500

    upload method detective

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -1,39 +1,38 @@
-# Replace in the "<???>" with the appropriate method (and arguments, if any).
-# Uncomment the calls to catch these methods red-handed.
+ Replace in the "<???>" with the appropriate method (and arguments, if any).
 
-# When there's more than one suspect who could have
-# committed the crime, add additional calls to prove it.
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"zom".insert(2, 'o')
+=> “zoom”
 
-# "zom".<???>
-# => “zoom”
+"enhance".center(20)
+ => "    enhance    "
 
-# "enhance".<???>
-# => "    enhance    "
+"Stop! You’re under arrest!".upcase
+ => "STOP! YOU’RE UNDER ARREST!"
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"the usual"..concat(" suspects")
+=> "the usual suspects"
 
-# "the usual".<???>
-#=> "the usual suspects"
+" suspects".prepend("the usual")
+=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+"The case of the disappearing last letter".chop
+ => "The case of the disappearing last lette"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
-
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".slice(0)
 # => "he mystery of the missing first letter"
+#will actually return 'T', but you can assign initial sentence to a
+#variable, and print new variable after using the bang operator.
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+ "Elementary,    my   dear        Watson!".squeeze
+=> "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+"z".ord
+=> 122 
 # (What is the significance of the number 122 in relation to the character z?)
+returns the ordinal value of a one character string based off the ordinal scale in Ruby
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"How many times does the letter 'a' appear in this string?".scan(/a/).count
+ => 4
