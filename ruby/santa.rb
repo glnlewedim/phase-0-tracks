c05d0758a8487ecd38a338ac8c3386b07822312f
Gloria Nlewedim <glnlewedim@davidson.edu>
2017-02-28 22:16:35 -0500

    fix mistake

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,3 @@
-#do the thing
 class Santa
   attr_reader :ethnicity
   attr_accessor :gender, :age
@@ -9,16 +8,17 @@ class Santa
     @age = 0
     puts "Initializing Santa instance..."
   end
-
+    
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
   
   def eat_milk_and_cookies(type)
-    puts "That was a good #{type} cookie"
+    puts "That was a good #{type}"
     type
   end
-  def jolliness (level)
+  
+  def jolliness(level)
     puts "My Jolly level is #{level}"
     level
   end
@@ -26,17 +26,10 @@ class Santa
   def Amount_of_presents (amount)
     puts "I usually give a #{amount} number of presents"
     amount
-#getter methods
-  def ethnicity
-    @ethnicity
   end
-    
-  def age
-    @age
-  end
-    
+
   #release2
-  def celebrate_birthday 
+  def celebrate_birthday
     @age += 1
   end
     
@@ -50,11 +43,12 @@ class Santa
     @reindeer_ranking
   end
     
-  def gender=(new_gender)
-    @gender = new_gender
-end
+  #setter method
+  #def gender=(new_gender)
+    #@gender = new_gender
+  #end
     
-  #release4 
+  #part of release4
   def rand_birthday
     @age = rand(140)
   end
@@ -62,26 +56,31 @@ end
 end
 
 #release 0
-santa_claus = Santa.new("female", "danish")
+santa_claus = Santa.new("male", "dutch")
 santa_claus.speak
-santa_claus.eat_milk_and_cookies("chocolate chip")
-santa_claus.celebrate_birthday
-santa_claus.get_mad_at ("Dasher")
-santa_claus.gender = "greek"
-santa_claus.age (50)
-santa_claus.ethnicity
+santa_claus.eat_milk_and_cookies("snickerdoodle")
+
+#do the thing
+p santa_claus.jolliness (50)
+p santa_claus.Amount_of_presents ("large")
+#release 2
+p santa_claus.celebrate_birthday
+p santa_claus.get_mad_at("Dasher")
+p santa_claus.gender = "greek"
+
+#additional tests for refactor
+p santa_claus.age
+p santa_claus.ethnicity
 
 #release 4
 santas = []
 genders = ["agender", "female", "male", "transexual", "bigender", "androgynous", "cisgender"]
-ethnicities = ["dutch", "black", "latino", "greek", "arabian", "chinese"]
+ethnicities = ["tai", "black", "latino", "greek", "arabian", "chinese"]
 50.times do |i|
   santas << Santa.new(genders.sample, ethnicities.sample)
-  puts "There are now #{santas.length} instances in the array."
+  puts "There are now #{santas.count} instances in the array."
   puts santas[i]
   puts santas[i].gender
   puts santas[i].ethnicity
   puts santas[i].rand_birthday
-
-end
 end
