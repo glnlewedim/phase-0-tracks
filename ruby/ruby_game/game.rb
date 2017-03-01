150d1d95e803097bca1f346b2fb16681cc947e0f
Gloria Nlewedim <glnlewedim@davidson.edu>
2017-03-01 17:50:39 -0500

    update repo

---
+++ ruby/ruby_game/game.rb
@@ -0,0 +1,10 @@
+#create a game (drive code will handle input and output)
+#your game will 
+#user inputs a word
+#other user attempts to guess word
+#limit guesses
+#guesses available linked to word length 
+#repeated guesses do not count
+#guessor recieves continual feedback
+#user gets congratulatory message or
+#user gets taungting message
